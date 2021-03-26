7df0ed9c7323e7a3b1d70f541ada675b5ea766f5
mobingsen <mobingsen@xiao100.com>
2021-03-26 13:54:41 +0800

    re

--- 第二章/re模块3.py
+++ 第二章/re模块3.py
@@ -1,4 +1,3 @@
-
 import requests
 import re
 import csv
@@ -8,11 +7,28 @@ headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) "
                   "Chrome/88.0.4324.182 Safari/537.36"
 }
-resp = requests.get(domain, verify=False) # verify=False去掉安全验证
-resp.encoding='gb2312'
+# verify=False去掉安全验证
+resp = requests.get(domain, verify=False)
+resp.encoding = 'gb2312'
 # print(resp.text)
 
-obj = re.compile(r"2020必看热片.*?<ul>.*?<a href='(?P<link_addr>.*?)' title=.*?</ul>", re.S)
+obj = re.compile(r"2020必看热片.*?<ul>(?P<ul>.*?)</ul>", re.S)
+obj2 = re.compile(r"<a href='(?P<href>.*?)'", re.S)
+obj3 = re.compile(r'◎片　　名(?P<movie>.*?)<br />.*?<td '
+                  r'style="WORD-WRAP: break-word" bgcolor="#fdfddf"><a href="(?P<download>.*?)">', re.S)
 result = obj.finditer(resp.text)
+child_href_list = []
 for it in result:
-    print(it.group())
+    ul = it.group('ul')
+    result2 = obj2.finditer(ul)
+    for itt in result2:
+        child_href = domain + itt.group('href').strip("/")
+        child_href_list.append(child_href)
+        # print(child_href)
+
+for href in child_href_list:
+    child_resp = requests.get(href, verify=False)
+    child_resp.encoding = 'gb2312'
+    result3 = obj3.search(child_resp.text)
+    print(result3.group("movie").strip())
+    print(result3.group("download"))
